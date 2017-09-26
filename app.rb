8c4188479f92cfc18b72e21f5b223c93a7d386da
MysticalMemory <31141553+MysticalMemory@users.noreply.github.com>
2017-09-26 15:05:17 -0400

    Add files via upload

--- app.rb
+++ app.rb
@@ -1,19 +1,33 @@
 # numbers = []
-couter = 0
 
+def user_input()
+	puts  "What's your favorite animal?"
+	animal = gets.chomp
+counter = 0
+	puts "What's your favorite apple?"
+	apple_type = gets.chomp
+counter = 0
+	puts "What's your favoite book?"
+	book = gets.chomp
+counter = 0
 100.times do 
 	puts counter
 	# numbers.push(counter)
-	counter = couter +1
-	if counter % 15 ==0 
-		puts "fizzbuzz"
+	counter = counter + 1
+	if counter % 15 ==0 	
+		puts animal
 	elsif counter % 3 == 0
-		puts "fizz"
+		puts apple_type
 	elsif counter % 5 == 0
-		puts "buzz"
+		puts book	
 	else
 		puts counter
 	end			
 end
+end
 
+
+1.times do
+	user_input()
+end
 # puts numbers
